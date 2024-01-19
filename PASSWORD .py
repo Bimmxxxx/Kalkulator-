f66bd11e0aa8e7611a66b20d7f0291d1f0901541
Bimmxxxx <153823154+Bimmxxxx@users.noreply.github.com>
2024-01-19 11:53:22 +0700

    Add files via upload

---
+++ PASSWORD .py
@@ -0,0 +1,20 @@
+
+print("Hello, World!")
+operasi = input('Masukan nama admin:')
+
+
+if operasi == 'bima':
+    
+    
+    
+sandi = input('Masukan sandi anda:')
+
+
+
+if sandi == 'bimma12345':
+    print('—' * 25)
+    print('anda berhasil masuk')
+    print('—' * 25)
+
+else:
+    print ('sandi salah atau nama admin salah')
